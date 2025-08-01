2161251adaf5f42e4d02867907122df02243ff0a
Nakagawa <rm93890@estudante.fieb.edu.br>
2025-08-01 15:16:08 +0000

    alteração sala

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('app/public'))
 app.set ('view engine', 'ejs')
 app.set('views', './app/views')
 
-var rotas = require('./app/rotes/router')
+var rotas = require('./app/routes/router')
 app.use('/', rotas)
 
 app.listen(port, ()=>{
